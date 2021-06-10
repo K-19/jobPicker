002a594bbd84e4dc8ba3430d6ed3059d76fc9aaf
K-19 <filinovich.sergey.work@gmail.com>
2021-06-10 17:16:25 +0300

    Initial commit

---
+++ src/main/java/by/k19/jobpicker/JobpickerApplication.java
@@ -0,0 +1,13 @@
+package by.k19.jobpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/k19/jobpicker/JobpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.k19.jobpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
